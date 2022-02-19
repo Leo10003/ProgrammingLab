bd2905a8c6f714345cd6b6e8668a246164621f98
Leonardo Piccinelli <94189023+Leo10003@users.noreply.github.com>
2022-02-19 17:23:46 +0000

    creazione della lista di liste

--- Lezione_4.py
+++ Lezione_4.py
@@ -1,7 +1,12 @@
 class CSVFile():
-    def init(self):
-        open('shampoo_sales.csv','r')
+    def __init__(self, name):
+        self.name=open(name,'r')
+    
+    def get_data(self):
+        self.c=list(self.name)
+        print(self.c)
+        self.name.close()
 
 
-file = CSVFile()
-print(file)
+file = CSVFile(name='shampoo_sales.csv')
+file.get_data()
--- esame.py
+++ esame.py
@@ -5,5 +5,10 @@ class CSVTimeSeriesFile():
     def __init__(self, name):
         self.name=open(name, 'r')
 
+    def printlist(self):
+        self.x=list(self.name)
+        print(self.x)
+
 time_series_file = CSVTimeSeriesFile(name='data.csv')
-time_series = time_series_file.get_data()
+time_series_file.printlist()
+#time_series = time_series_file.get_data()
