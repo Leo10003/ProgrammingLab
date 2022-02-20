db2b123a4895966cff5cd9e22bed8d4c99166f6e
Leonardo Piccinelli <94189023+Leo10003@users.noreply.github.com>
2022-02-20 14:03:41 +0000

    Copiatura dei dati per ogni mese dei 2 anni consecutivi

--- esame.py
+++ esame.py
@@ -1,3 +1,5 @@
+import re
+
 class ExamException(Exception):
     pass
 
@@ -8,18 +10,38 @@ class CSVTimeSeriesFile():
     def get_data(self):
         self.x=[]
         for line in self.name:
-            self.data=line.split(',')
-            self.data[1]=self.data[1].strip()
+            self.data=re.split(r'-|,',line)
+            self.data[2]=self.data[2].strip()
+            self.data[0]=int(self.data[0])
+            self.data[1]=int(self.data[1])
+            self.data[2]=int(self.data[2])
             self.x.append(self.data)
-            
-
         return self.x
 
 def detect_similar_monthly_variations(time_series, years):
+    media_anno_1=[]
+    media_anno_2=[]
+    mesi_anno_1=[]
+    mesi_anno_2=[]
+    mese=0
     for i in range(2):
         print(years[i] , '\n')
-        if years[i] in time_series:
-            print('1\n')
+        index=[a for a, lst in enumerate(time_series) if years[i] in lst][0]
+        for list in time_series:
+            if years[i] in list:
+                if(i==0):
+                    mesi_anno_1.append(time_series[index][2])
+                    print('Mese ',mese+1,': ',mesi_anno_1[mese])
+                    index+=1
+                    mese+=1
+                elif(i==1):
+                    mesi_anno_2.append(time_series[index][2])
+                    print('Mese ',mese+1,': ',mesi_anno_2[mese])
+                    index+=1
+                    mese+=1
+        mese=0
+
+
 
 time_series_file = CSVTimeSeriesFile(name='data.csv')
 time_series = time_series_file.get_data()
